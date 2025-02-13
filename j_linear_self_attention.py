cc6b5527ce81df9d06c6f720b148e8f298ee3c0c
Rais Kazi <meetrais@gmail.com>
2025-02-12 20:57:48 -0600

    Added code for linear self attention and masking.

---
+++ j_linear_self_attention.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+
+inputs = torch.tensor(
+    [[0.43, 0.15, 0.89], # Your
+     [0.55, 0.87, 0.66], # journey 
+     [0.57, 0.85, 0.64], # starts 
+     [0.22, 0.58, 0.33], # with
+     [0.77, 0.25, 0.10], # one
+     [0.05, 0.80, 0.55]] # step
+)
+
+d_in = inputs.shape[1]
+d_out = 2
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
+print("\n################################################################\n")
+
+#Calculate attention weights
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs)
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+print(attn_weights)
+print("\n################################################################\n")
+
+#Masking
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+print("\n################################################################\n")
+
+masked_simple = attn_weights*mask_simple
+print(masked_simple)
+print("\n################################################################\n")
+
+#Normalization
+row_sums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple / row_sums
+print(masked_simple_norm)
+print("\n################################################################\n")
+
+#Negative infinity masking
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+print(masked)
+print("\n################################################################\n")
+
+#Calculate attention weights
+attn_weights = torch.softmax(masked / keys.shape[-1]**0.5, dim=1)
+print(attn_weights)
+print("\n################################################################\n")
+
+#Apply additional dropout mask - This part is confusing
+torch.manual_seed(123)
+dropout = torch.nn.Dropout(0.5)
+example = torch.ones(6, 6)
+print(dropout(example))
+print("\n################################################################\n")
+
+torch.manual_seed(123)
+print(dropout(attn_weights))
+
+batch = torch.stack((inputs, inputs), dim=0)
+print("\nbatch.shape: ", batch.shape)
+print("\n################################################################\n")
+
+
+
