e5b70babefd6f9629ef9e78cc3e3d5bda7cde6be
Rais Kazi <meetrais@gmail.com>
2025-02-13 16:04:18 -0600

    Code added for shortcut deep neural network.

---
+++ q_shortcut_dnn.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import p_layernorm_gelu_feedforward_deep_dive as p
+
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), p.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), p.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), p.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), p.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), p.GELU())
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+    
+layer_sizes = [3, 3, 3, 3, 3, 1]
+sample_input = torch.tensor([[1., 0., -1.]])
+torch.manual_seed(123)
+model_without_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=False
+)
+
+def print_gradients(model, x):
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+print_gradients(model_without_shortcut, sample_input)
+print("\n################################################################\n")
+
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=True
+)
+print_gradients(model_with_shortcut, sample_input)
+print("\n################################################################\n")
+
