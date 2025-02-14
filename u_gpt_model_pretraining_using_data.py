27ec91f7ed46795e590691541b6ed09ae3b7c2b7
Rais Kazi <meetrais@gmail.com>
2025-02-14 17:54:12 -0600

    Added code to pretrain model using data, load gpt2 model weight.

---
+++ u_gpt_model_pretraining_using_data.py
@@ -0,0 +1,283 @@
+import torch
+import tiktoken
+from e_data_prepration import create_dataloader_v1
+from s_gpt_model import GPTModel
+from t_gpt_model_pretraining import text_to_token_ids, token_ids_to_text, generate_text_simple
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)
+    loss = torch.nn.functional.cross_entropy(
+        logits.flatten(0, 1), target_batch.flatten()
+    )
+    return loss
+
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(
+            train_loader, model, device, num_batches=eval_iter
+        )
+        val_loss = calc_loss_loader(
+            val_loader, model, device, num_batches=eval_iter
+        )
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model=model, idx=encoded,
+            max_new_tokens=50, context_size=context_size
+        )
+        decoded_text = token_ids_to_text(token_ids, tokenizer)
+        print(decoded_text.replace("\n", " "))
+    model.train()
+
+def train_model_simple(model, train_loader, val_loader,
+                      optimizer, device, num_epochs,
+                      eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+
+            #Backpropagation
+            loss.backward()
+            #Update weights
+            optimizer.step()
+
+            tokens_seen += input_batch.numel()
+            global_step += 1
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, "
+                      f"Val loss {val_loss:.3f}"
+                )
+                generate_and_print_sample(
+                    model, tokenizer, device, start_context
+                )
+    return train_losses, val_losses, track_tokens_seen
+
+def print_sampled_tokens(probas):
+    torch.manual_seed(123)
+    sample = [torch.multinomial(probas, num_samples=1).item()
+             for i in range(1_000)]
+    sampled_ids = torch.bincount(torch.tensor(sample))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+def generate(model, idx, max_new_tokens, context_size,
+            temperature=0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+            logits = logits[:, -1, :]
+            if top_k is not None:
+                top_logits, _ = torch.topk(logits, top_k)
+                min_val = top_logits[:, -1]
+                logits = torch.where(
+                    logits < min_val,
+                    torch.tensor(float('-inf')).to(logits.device),
+                    logits
+                )
+            if temperature > 0.0:
+                logits = logits / temperature
+                probs = torch.softmax(logits, dim=-1)
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+            if idx_next == eos_id:
+                break
+            idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+if __name__ == "__main__":
+    file_path = "the-verdict.txt"
+    with open(file_path, "r", encoding="utf-8") as file:
+        text_data = file.read()
+    total_characters = len(text_data)
+    tokenizer = tiktoken.get_encoding("gpt2")
+    total_tokens = len(tokenizer.encode(text_data))
+    print("Characters:", total_characters)
+    print("Tokens:", total_tokens)
+    print("\n##############################################\n")
+
+    train_ratio = 0.90
+    split_idx = int(train_ratio * len(text_data))
+    train_data = text_data[:split_idx]
+    val_data = text_data[split_idx:]
+    print("\n##############################################\n")
+
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,
+        "context_length": 256,
+        "emb_dim": 768,
+        "n_heads": 12,
+        "n_layers": 12,
+        "drop_rate": 0.1,
+        "qkv_bias": False
+    }
+
+    torch.manual_seed(123)
+    train_loader = create_dataloader_v1(
+        train_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0
+    )
+    val_loader = create_dataloader_v1(
+        val_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=False,
+        shuffle=False,
+        num_workers=0
+    )
+
+    print("Train loader:")
+    for x, y in train_loader:
+        print(x.shape, y.shape)
+    print("\n##############################################\n")
+
+    print("\nValidation loader:")
+    for x, y in val_loader:
+        print(x.shape, y.shape)
+    print("\n##############################################\n")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = GPTModel(GPT_CONFIG_124M)
+    model.eval()
+    model.to(device)
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device)
+        val_loss = calc_loss_loader(val_loader, model, device)
+    print("Training loss:", train_loss)
+    print("Validation loss:", val_loss)
+    print("\n##############################################\n")
+
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    model.to(device)
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=0.0004, weight_decay=0.1
+    )
+    num_epochs = 10
+    train_losses, val_losses, tokens_seen = train_model_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+        start_context="Every effort moves you", tokenizer=tokenizer
+    )
+
+    model.to("cpu")
+    model.eval()
+    tokenizer = tiktoken.get_encoding("gpt2")
+    token_ids = generate_text_simple(
+        model=model,
+        idx=text_to_token_ids("Every effort moves you", tokenizer),
+        max_new_tokens=25,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+    print("\n##############################################\n")
+
+
+    vocab = {
+        "closer": 0,
+        "every": 1,
+        "effort": 2,
+        "forward": 3,
+        "inches": 4,
+        "moves": 5,
+        "pizza": 6,
+        "toward": 7,
+        "you": 8
+    }
+    inverse_vocab = {v: k for k, v in vocab.items()}
+
+    next_token_logits = torch.tensor([4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79])
+    probas = torch.softmax(next_token_logits, dim=0)
+    next_token_id = torch.argmax(probas).item()
+    print(inverse_vocab[next_token_id])
+
+    torch.manual_seed(123)
+    next_token_id = torch.multinomial(probas, num_samples=1).item()
+    print(inverse_vocab[next_token_id])
+    print("\n##############################################\n")
+
+    print_sampled_tokens(probas)
+    print("\n##############################################\n")
+
+    top_k = 3
+    top_logits, top_pos = torch.topk(next_token_logits, top_k)
+    print("Top logits:", top_logits)
+    print("Top positions:", top_pos)
+    print("\n##############################################\n")
+
+    new_logits = torch.where(condition=next_token_logits < top_logits[-1],
+                            input=torch.tensor(float('-inf')),
+                            other=next_token_logits)
+    print(new_logits)
+    print("\n##############################################\n")
+
+    topk_probas = torch.softmax(new_logits, dim=0)
+    print(topk_probas)
+    print("\n##############################################\n")
+    
+    torch.manual_seed(123)
+    token_ids = generate(
+        model=model,
+        idx=text_to_token_ids("Every effort moves you", tokenizer),
+        max_new_tokens=15,
+        context_size=GPT_CONFIG_124M["context_length"],
+        top_k=25,
+        temperature=1.4
+    )
+    #Not quite right yet, but better than garbage.
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+    print("\n##############################################\n")
+
+    #Lets save weights, to be used in the next file(v_load_gpt2_model_weights.py).
+    torch.save(model.state_dict(), "model.pth")
