8e833030df6df3cc966f98e86a7401965c8c542c
Rais Kazi <meetrais@gmail.com>
2025-02-16 11:29:06 -0600

    Added code to finetune gpt model using instruction tuning.

--- u_gpt_model_pretraining_using_data.py
+++ u_gpt_model_pretraining_using_data.py
@@ -86,9 +86,9 @@ def train_model_simple(model, train_loader, val_loader,
                       f"Train loss {train_loss:.3f}, "
                       f"Val loss {val_loss:.3f}"
                 )
-                generate_and_print_sample(
-                    model, tokenizer, device, start_context
-                )
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
     return train_losses, val_losses, track_tokens_seen
 
 def print_sampled_tokens(probas):
